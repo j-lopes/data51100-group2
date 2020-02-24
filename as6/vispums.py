8a67adca51853c18bf27d36dec44efd992c6a106
Geoff Stewart <geoff.stewart@oracle.com>
2020-02-24 11:21:40 -0800

    add the starting point for assignment 6

---
+++ as6/vispums.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+This program computes loads the PUMS dataset and prepares some visualizations based on the data
+Students:   Jenna Lopes, Geoffrey Stewart, Ryan Wade
+Date:       Feb. 24, 2020
+Course:     DATA-51100-002: 	Statistical Programming
+Semester:   Spring 2020
+Assignment: PROGRAMMING ASSIGNMENT #6
+
+"""
+
+
+def main():
+    # log the required output header
+    print('DATA-51100, Spring 2020')
+    print('NAMES: Jenna Lopes, Geoffrey Stewart, Ryan Wade')
+    print('PROGRAMMING ASSIGNMENT #6')
+    print('')
+
+
+if __name__ == "__main__":
+    main()
